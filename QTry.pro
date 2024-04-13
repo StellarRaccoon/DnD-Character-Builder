7298e22849e5af64e848075fa458aa3b4e7d6504
Caden Joergens <58443587+StellarRaccoon@users.noreply.github.com>
2024-04-13 10:51:36 -0400

    Show Racial Traits in dialog. Dialog Buttons in wrong place

--- QTry.pro
+++ QTry.pro
@@ -9,18 +9,18 @@ CONFIG += c++17
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    dialog.cpp \
+    dialogTraits.cpp \
     jsonmanipulation.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
-    dialog.h \
+    dialogTraits.h \
     jsonmanipulation.h \
     mainwindow.h
 
 FORMS += \
-    dialog.ui \
+    dialogTraits.ui \
     mainwindow.ui
 
 # Default rules for deployment.
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,7 +2,7 @@
 #include "ui_mainwindow.h"
 //#include "secdialog.h"
 #include<QLabel>
-#include "dialog.h"
+#include "dialogTraits.h"
 #include<QNetworkReply>
 #include<QDebug>
 #include<QJsonDocument>
@@ -11,55 +11,29 @@
 #include<QTreeWidget>
 #include<QTreeWidgetItem>
 #include"jsonmanipulation.h"
+
+/**
+ * Main Window Consturctor
+*/
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    /*
-    ui->progressBar->setValue(30);
-    connect(ui->horizontalSlider,SIGNAL(valueChanged(int)), ui->progressBar, SLOT(setValue(int)));
-    //disconnect(ui->horizontalSlider,SIGNAL(valueChanged(int)), ui->progressBar, SLOT(setValue(int)));
-    */
-
     std::vector<std::string> raceNames {"Elf", "Human", "Dwarf", "Halfling", "Gnome"};
-    //connect(&manager, &QNetworkAccessManager::finished, this, &MainWindow::onManagerFinished);
-/*
-    QNetworkRequest request(QUrl(urlBase.arg("races")));
-
-    QNetworkReply *reply =manager.get(request);
-
-    QEventLoop loop;
-    QObject::connect(reply,&QNetworkReply::finished,&loop,&QEventLoop::quit);
-    loop.exec();
-
-    if(reply->error()==QNetworkReply::NoError){
-        qDebug()<<"Error: "<<reply->errorString();
-        QString Response = reply->readAll();
-        qDebug()<<"API Response: "<<Response;
-
-    }else{
-        qDebug()<<"Error: "<<reply->errorString();
-    }
-*/
-    //fetchData("races");
 
     QJsonArray jsonArrayRaceNames = {};
     QJsonDocument doc(jsonArrayRaceNames);
-    //QJsonArray jsonRaces = {};
+
     //should probably do something if these fail (like try again)
     qDebug()<<jason.fetchData("/api/races", doc);
-    //JsonManipulation *jason = new JsonManipulation();
-
     jason.getNamesFromJson(doc,jsonArrayRaceNames);
-    //ui->textEdit->setPlainText(doc["results"].toString());
 
     //get the names and urls from each to get more data
     ui->treeWidget->setColumnCount(2);
     ui->treeWidget->setColumnHidden(1,true);
     ui->treeWidget->setHeaderLabels(QStringList()<<"Hey"<<"Cowboy");
-
 
     AddRoot("Hello", "World");
 
@@ -70,39 +44,21 @@ MainWindow::MainWindow(QWidget *parent)
         QTreeWidgetItem *raceRoot= new QTreeWidgetItem(ui->treeWidget);
         raceRoot->setText(0,raceName);
         raceRoot->setText(1,raceUrl);
-        //QListWidgetItem *newItem=new QListWidgetItem();
-        //newItem->setText(raceName);
-        //newItem->setWhatsThis(raceUrl);//theres gotta be a better way to store some hidden data
         qDebug()<<"url:"<<raceRoot->text(0);
-        //ui->treeWidget->addItem(raceName);
-
-/*TODO: Use correct data in dialouge when clicked*/
-        //should only do this if clicked
-        //QJsonDocument raceData = QJsonDocument();
-        //fetchData(raceUrl, raceData);
-
     }
 }
 
+/**
+ * Main Window destructor
+*/
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
-
-/*
-void MainWindow::on_pushButton_clicked()
-{
-    // SecDialog secDialog;
-    // secDialog.setModal(true);
-    // secDialog.exec();
-    hide();
-    secDialog=new SecDialog(this);
-    secDialog->show();
-
-}
+/**
+ * Add Root to tree in the widget page
 */
-
 void MainWindow::AddRoot(QString name, QString description){
     QTreeWidgetItem *itm = new QTreeWidgetItem(ui->treeWidget);
     itm->setText(0, name); //name in column0
@@ -110,6 +66,10 @@ void MainWindow::AddRoot(QString name, QString description){
     ui->treeWidget->addTopLevelItem(itm);
 
 }
+
+/**
+ * Add a child to a tree root
+*/
 void MainWindow::addChild(QTreeWidgetItem *parent, QString name, QString description){
     QTreeWidgetItem *itm = new QTreeWidgetItem(ui->treeWidget);
     itm->setText(0, name); //name in column0
@@ -131,10 +91,15 @@ void MainWindow::on_treeWidget_itemClicked(QTreeWidgetItem *item, int column)
     jason.fetchData(item->text(1),dataDoc);
 
     //convert doc to a JsonObject
-
-    QJsonObject dataObj = dataDoc.object();
+    QString raceName = item->text(0);
+    //QJsonObject dataObj = dataDoc.object();
     //create a new dialog box object
-    Dialog infoDialog(this,&dataObj);
+    QJsonDocument traitsDoc;
+    QJsonArray traitsArray = {};
+    jason.fetchData(item->text(1).append("/traits"),traitsDoc);
+    jason.getNamesFromJson(traitsDoc, traitsArray);
+    qDebug()<<item->text(1).append("/traits");
+    Dialog infoDialog(this,raceName, traitsArray);
     infoDialog.setModal(true); //blocks interactions with main windows
     infoDialog.exec(); //launch the dialog box
 }
